a11732e0d25de292d2a7ce87beda5d98ba58cf40
GitAcrown <Acrown@outlook.fr>
2017-05-01 02:08:13 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,14 +2,13 @@ var perClick = 1.0;
 var multiplier = 1.0;
 var total = 0;
 var totalClicks = 0;
-var audio = document.createElement("audio");
 
 function addValue() {
 	total = total + perClick * multiplier;
 	totalClicks ++;
-	document.title = totalClicks + " Total Clicks";
+	document.title = totalClicks + " Clicks au total";
 	var totalDisplay = document.getElementById("total-display");
-	totalDisplay.innerHTML = totalee;
+	totalDisplay.innerHTML = total;
 	audio.volume = 1.0;
 }
 
