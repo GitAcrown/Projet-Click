82d5fd342a8b66912536568d184ad96f2f2fdac6
GitAcrown <Acrown@outlook.fr>
2017-05-01 02:19:45 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,11 +2,12 @@ var perClick = 1.0;
 var multiplier = 1.0;
 var total = 0;
 var totalClicks = 0;
+var totalDisplay = "Aucun click";
 
 function addValue() {
 	total = total + perClick * multiplier;
 	totalClicks ++;
-	document.title = totalClicks + " Clicks au total">"Aucun click"<;
+	document.title = totalClicks + " Clicks au total";
 	var totalDisplay = document.getElementById("total-display");
 	totalDisplay.innerHTML = total;
 }
